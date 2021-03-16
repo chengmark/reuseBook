7285395f49535dec1ba5cfa97e5f2aa3d530280f
MarkCheng <mocheng26@gmail.com>
2021-03-17 01:24:22 +0800

    build: update server tsconfig & hide passport related code

--- api/index.ts
+++ api/index.ts
@@ -3,9 +3,9 @@ import bodyparser from 'body-parser'
 import http from 'http'
 import { Routes } from './routes'
 import { UserRoutes } from './routes/user'
-import { AuthRoutes } from './routes/auth'
+// import { AuthRoutes } from './routes/auth'
 // import initPassport from './authentication'
-import middlewares from './middlewares'
+// import middlewares from './middlewares'
 import path from 'path'
 
 const app: express.Application = express()
@@ -25,10 +25,10 @@ const router = express.Router()
 routes.push(new UserRoutes(router))
 
 // create auth routes to the router
-routes.push(new AuthRoutes(router))
+// routes.push(new AuthRoutes(router))
 
 // use middlewares
-app.use(middlewares)
+// app.use(middlewares)
 
 // all routes start with '/api'
 app.use('/api', router)
