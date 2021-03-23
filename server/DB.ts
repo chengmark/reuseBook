d9e04ac06ce32e45c8fe92add2b4dab0ab8e06db
MarkCheng <mocheng26@gmail.com>
2021-03-23 17:43:12 +0800

    fix: db credential exposure

--- server/DB.ts
+++ server/DB.ts
@@ -18,7 +18,3 @@ const DB = {
 }
 
 export default DB
-// csci3100-A6
-// LFlzuQXtO30IuCRG
-
-// mongodb+srv://csci3100-A6:LFlzuQXtO30IuCRG@csci3100-southeast-1.r54cz.mongodb.net/csci3100?retryWrites=true&w=majority
