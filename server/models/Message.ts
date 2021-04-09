e4fa8b03d53bf196f0abb8cd75ac6ce7d1223dac
rkumar0099 <68481024+rkumar0099@users.noreply.github.com>
2021-04-09 10:45:29 +0500

    Create Message.ts

---
+++ server/models/Message.ts
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose'
+
+const Message = new mongoose.Schema(
+   {
+       body: {type: String},
+       author: {type: String},
+       date: {type: Date, default: Date.now}
+   },
+    {timestamps: {createdAt: 'createdAt'}}
+
+)
+
+export default mongoose.model('Message', Message)
